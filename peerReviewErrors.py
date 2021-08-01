4ded0cf262b2b4abcb72dfd973db038a89ca1c3c
BoMcDonald <88251564+BoMcDonald@users.noreply.github.com>
2021-07-31 20:32:51 -0500

    Original Error Code Top with Corrected Code Bottom
    The original code for our assignment is up top with the corrected version (with notes) is underneath

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -53,3 +53,63 @@ while playAgain = 'yes' or playAgain = 'y':
 		print("Thanks for planing")
 
 		      
+--------------------------------------------------------------------------------------------------
+#CORRECTED VERSION
+		      
+import random
+import time 
+
+def displayIntro():
+	#Not sure if this is a correction but I brought the text to the front of the line so the message is uniform on each line when displayed (1)
+	print('''You are in a land full of dragons. In front of you, 
+you see two caves. In one cave, the dragon is friendly 
+and will share his treasure with you. The other dragon 
+is greedy and hungry, and will eat you on sight.''')
+print()
+	
+def chooseCave():
+	cave = ''
+	#Corrected unnecessary indention in the next line for the while statement (2)
+	while cave != '1' and cave != '2':
+		print('Which cave will you into? (1 or 2)')
+		cave = input()
+	#Corrected the variable error (logical error) to change 'caves' to 'cave' as 'caves' is not defined in this exercise (3)
+	return cave
+
+def checkCave(chosenCave):
+	print('You approach the cave...')
+	#sleep for 2 seconds
+	time.sleep(2)
+	print('It is dark and spooky...')
+	#sleep for 2 seconds
+	# Corrected the time below from time.sleep(3) to time.sleep(2) since the instruction above says sleep 2 seconds (4)
+	time.sleep(2)
+	print('A large dragon jumps out in front of you! He open his jaws and...')
+	print()
+	#sleep for 2 seconds 
+	time.sleep(2)
+	
+	friendlyCave = random.randint(1, 2)
+	
+	if chosenCave == str(friendlyCave):
+		print('Gives you his treasure!')
+	else:
+		#Corrected the print statement to include parenthesis that were not included (syntax error) (5)
+		print('Gobbles you down in one bite!')
+
+#Added 'y' for accepted playAgain input (6)
+playAgain = 'yes' and 'y'
+# Corrected the conditional statement being '==' instead of '=' (syntax error) (7)
+while playAgain == 'yes' or playAgain == 'y':
+	displayIntro()
+	# Corrected variable error to now be chooseCave() as choosecave() is not defined (logical error) (8)
+	caveNumber = chooseCave()
+	checkCave(caveNumber)
+
+	print('Do you want to play again? (yes or no)')
+	playAgain = input()
+	
+# Corrected indention for the if statement (syntax error) (9)
+if playAgain == 'no' or playAgain == 'n':
+	# Corrected the spelling error of 'planing' to 'playing' (10)
+	print('Thanks for playing')
